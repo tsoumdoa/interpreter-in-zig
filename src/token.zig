68456e522a8a9c5e96b3c60a4bbeb38571c214a4
tsoumdoa <8blondes.swerves@icloud.com>
2025-03-24 22:21:13 +0800

    fixed testing and delete root

--- src/token.zig
+++ src/token.zig
@@ -1,3 +1,4 @@
+<<<<<<< ours
 const std = @import("std");
 const testing = std.testing;
 
@@ -82,3 +83,44 @@ test "lookupIdent" {
     try std.testing.expectEqual(TokenType.LET, lookupIdent("let"));
     try std.testing.expectEqual(TokenType.FUNCTION, lookupIdent("fn"));
 }
+|||||||
+=======
+const std = @import("std");
+const testing = std.testing;
+
+const TokenType = []const u8;
+pub const Token = struct {
+    Type: TokenType,
+    Literal: []const u8,
+};
+
+pub const ILLEGAL = "ILLEGAL";
+pub const EOF = "EOF";
+
+pub const IDENT = "IDENT"; //add, foobar, x, y,...
+pub const INT = "INT"; //123, 0, 1,...
+
+pub const ASSIGN = "=";
+pub const PLUS = "+";
+
+pub const COMMA = ",";
+pub const SEMICOLON = ";";
+
+pub const LPAREN = "(";
+pub const RPAREN = ")";
+
+pub const LBRACE = "{";
+pub const RBRACE = "}";
+
+pub const FUNCTION = "FUNCTION";
+pub const LET = "LET";
+
+test "can initialize token" {
+    // thee is no type safety
+    const token = Token{
+        .Type = "INT",
+        .Literal = "hello",
+    };
+    try std.testing.expectEqualStrings("hello", token.Literal);
+}
+>>>>>>> theirs

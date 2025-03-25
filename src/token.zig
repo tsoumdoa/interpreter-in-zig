c514a1914522400896730a85d6215d5dfc367b00
tsoumdoa <8blondes.swerves@icloud.com>
2025-03-25 22:16:15 +0800

    next token (=+(){},;) test added

--- src/token.zig
+++ src/token.zig
@@ -1,37 +1,38 @@
 const std = @import("std");
 const testing = std.testing;
 
-const TokenType = []const u8;
+// const TokenType = []const u8;
+pub const TokenType = enum {
+    ILLEGAL,
+    EOF,
+
+    IDENT,
+    INT,
+
+    ASSIGN,
+    PLUS,
+
+    COMMA,
+    SEMICOLON,
+
+    LPAREN,
+    RPAREN,
+
+    LBRACE,
+    RBRACE,
+
+    FUNCTION,
+    LET,
+};
 pub const Token = struct {
     Type: TokenType,
     Literal: []const u8,
 };
 
-pub const ILLEGAL = "ILLEGAL";
-pub const EOF = "EOF";
-
-pub const IDENT = "IDENT"; //add, foobar, x, y,...
-pub const INT = "INT"; //123, 0, 1,...
-
-pub const ASSIGN = "=";
-pub const PLUS = "+";
-
-pub const COMMA = ",";
-pub const SEMICOLON = ";";
-
-pub const LPAREN = "(";
-pub const RPAREN = ")";
-
-pub const LBRACE = "{";
-pub const RBRACE = "}";
-
-pub const FUNCTION = "FUNCTION";
-pub const LET = "LET";
-
 test "can initialize token" {
     // thee is no type safety
     const token = Token{
-        .Type = "INT",
+        .Type = TokenType.IDENT,
         .Literal = "hello",
     };
     try std.testing.expectEqualStrings("hello", token.Literal);

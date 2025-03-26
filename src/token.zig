81d1420b901a180dc1a2c9835e8c032fcf840c79
tsoumdoa <8blondes.swerves@icloud.com>
2025-03-26 20:06:48 +0800

    minor fix

--- src/token.zig
+++ src/token.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const testing = std.testing;
 
-// const TokenType = []const u8;
 pub const TokenType = enum {
     ILLEGAL,
     EOF,
@@ -24,16 +23,36 @@ pub const TokenType = enum {
     FUNCTION,
     LET,
 };
+
+pub const Keywords = [_]KeywordType{
+    .{ .keyword = "let", .tokenType = TokenType.LET },
+    .{ .keyword = "fn", .tokenType = TokenType.FUNCTION },
+};
+
+pub const KeywordType = struct {
+    keyword: []const u8,
+    tokenType: TokenType,
+};
+
+pub fn lookupIdent(keyword: []const u8) TokenType {
+    for (Keywords) |kw| {
+        if (std.mem.eql(u8, kw.keyword, keyword)) {
+            return kw.tokenType;
+        }
+    }
+    return TokenType.IDENT;
+}
+
 pub const Token = struct {
     Type: TokenType,
     Literal: []const u8,
 };
 
 test "can initialize token" {
-    // thee is no type safety
     const token = Token{
         .Type = TokenType.IDENT,
         .Literal = "hello",
     };
     try std.testing.expectEqualStrings("hello", token.Literal);
+    try std.testing.expectEqual(TokenType.IDENT, lookupIdent(token.Literal));
 }

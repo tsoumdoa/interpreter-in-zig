ba6c44939ca756235daa34c32e5a3441ea40b86c
tsoumdoa <8blondes.swerves@icloud.com>
2025-04-14 20:34:51 +0800

    moekey repl added!

--- src/repl.zig
+++ src/repl.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const lexer = @import("lexer.zig");
+const parser = @import("parser.zig");
 const Reader = std.io.GenericReader;
 const Writer = std.io.GenericWriter;
 const stdin = std.io.getStdIn().reader();
@@ -9,6 +10,9 @@ const PROMPT = ">> ";
 
 pub fn start() !void {
     var buf: [1024]u8 = undefined;
+
+    var gpa: std.heap.GeneralPurposeAllocator(.{}) = .{};
+    const alloc = gpa.allocator();
 
     while (true) {
         try stdout.print(PROMPT, .{});
@@ -20,14 +24,41 @@ pub fn start() !void {
                 return;
             }
             var lex = lexer.Lexer.init(l);
-
-            while (true) {
-                const tok = lex.nextToken();
-                if (tok.Type == .EOF) {
-                    break;
+            var p = parser.Parser.init(&lex, alloc);
+            defer p.deinit();
+            var program = p.parseProgram() catch |err| {
+                try stdout.print("{s}\n", .{monkey});
+                try stdout.print("Woops, we run into some monkey business here!:\n", .{});
+                try stdout.print("error: {}\n", .{err});
+                continue;
+            };
+            defer program.deinit();
+            if (p.errors.items.len != 0) {
+                for (p.errors.items) |err| {
+                    try stdout.print("{s}\n", .{monkey});
+                    try stdout.print("Woops, we run into some monkey business here!:\n", .{});
+                    try stdout.print("error: {s}\n", .{err});
                 }
-                try stdout.print("Type: {any} - Literal:{s}\n", .{ tok.Type, tok.Literal });
+                continue;
             }
+
+            const out = try program.string();
+            defer out.deinit();
+
+            try stdout.print("{s}\n", .{out.items});
         }
     }
 }
+
+const monkey =
+    \\   .--.  .-"     "-.  .--.
+    \\  / .. \/  .-. .-.  \/ .. \
+    \\ | |  '|  /   Y   \  |'  | |
+    \\ | \   \  \ 0 | 0 /  /   / |
+    \\  \ '- ,\.-"""""""-./, -' /
+    \\   ''-' /_   ^ ^   _\ '-''
+    \\       |  \._   _./   |
+    \\       \   \ '~' /   /
+    \\        '._ '-=-' _.'
+    \\           '-----'
+;
